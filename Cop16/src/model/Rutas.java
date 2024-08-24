2ba035143d30dbbf3ad3a074b042621cd9e2f042
JuanDb20 <JuandiegonacionalU2023@gmail.com>
2024-08-24 08:39:53 -0500

    Actualización

--- Cop16/src/model/Rutas.java
+++ Cop16/src/model/Rutas.java
@@ -1,26 +1,72 @@
 import java.util.Scanner;
-/**
- * @author Juan Diego Balanta Molina A00407538
- * La clase {@code Rutas} es una aplicación que permite a un voluntario ingresar la información
- * de una ruta ecológica para la COP 16 en Cali, incluyendo la ruta seleccionada, el número de participantes,
- * guías, condiciones meteorológicas, y el cálculo del número de buses necesarios para el transporte.
- * 
- */
+
 public class Rutas {
+
+    /**
+     * Método principal que ejecuta la aplicación.
+     * Solicita datos del usuario, selecciona la ruta, solicita y verifica condiciones meteorológicas,
+     * calcula el número de buses necesarios, y muestra toda la información.
+     * 
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita al usuario su nombre y cédula, y muestra un saludo de bienvenida.
+        String nombre = solicitarNombre(scanner);
+        String cedula = solicitarCedula(scanner);
+        saludarUsuario(nombre);
 
-        System.out.println(
-                "# Bienvenido voluntario a la aplicación de Interacción de Rutas Ecológicas COP 16 Cali - Colombia. ¿Cuál es tu nombre?");
-        String nombre = scanner.nextLine();
+        String[] detallesRuta = seleccionarRuta(scanner);
+        mostrarDetallesRuta(detallesRuta);
+
+        int participantes = solicitarCantidad(scanner, "participantes");
+        int guias = solicitarCantidad(scanner, "guías");
+        int totalPersonas = participantes + guias;
+
+        solicitarYVerificarCondicionesMeteorologicas(scanner);
+
+        calcularYMostrarBuses(totalPersonas);
+    }
+
+    /**
+     * Solicita al usuario su nombre.
+     * 
+     * @param scanner El Scanner utilizado para leer la entrada del usuario.
+     * @return El nombre ingresado por el usuario.
+     */
+    public static String solicitarNombre(Scanner scanner) {
+        System.out.println("# Bienvenido voluntario a la aplicación de Interacción de Rutas Ecológicas COP 16 Cali - Colombia. ¿Cuál es tu nombre?");
+        return scanner.nextLine();
+    }
+
+    /**
+     * Solicita al usuario su cédula.
+     * 
+     * @param scanner El oScanner utilizado para leer la entrada del usuario.
+     * @return La cédula ingresada por el usuario.
+     */
+    public static String solicitarCedula(Scanner scanner) {
         System.out.println("# Por favor, digita tu cédula.");
-        String cedula = scanner.nextLine();
-        System.out.println("# ¡Bienvenido, " + nombre + "!");
-        
-        // Solicita al usuario seleccionar una ruta y muestra los detalles de la ruta seleccionada.
+        return scanner.nextLine();
+    }
 
+    /**
+     * Muestra un saludo al voluntario.
+     * 
+     * @param nombre El nombre del usuario que se va a saludar.
+     */
+    public static void saludarUsuario(String nombre) {
+        System.out.println("# ¡Bienvenido, " + nombre + "!");
+    }
+
+    /**
+     * Solicita al usuario que seleccione una ruta y devuelve los detalles de la misma.
+     * 
+     * @param scanner El Scanner utilizado para leer la entrada del usuario.
+     * @return Un arreglo de Strings con los detalles de la ruta seleccionada
+     *         [punto de encuentro, hora de inicio, hora de fin].
+     */
+    public static String[] seleccionarRuta(Scanner scanner) {
         System.out.println("# ¿Qué ruta registrarás el día de hoy?");
         System.out.println("1. Ruta de los Farallones");
         System.out.println("2. Ruta del Oriente");
@@ -28,12 +74,9 @@ public class Rutas {
         int ruta = scanner.nextInt();
         scanner.nextLine();
 
-        // Datos de la ruta
         String puntoEncuentro = "";
         String horaInicio = "";
         String horaFin = "";
-
-        // Asigna los datos de la ruta según la opción seleccionada (Swtich alternativa al if-else)
 
         switch (ruta) {
             case 1:
@@ -53,38 +96,63 @@ public class Rutas {
                 break;
             default:
                 System.out.println("# Opción de ruta inválida.");
-                return;
+                return new String[]{"", "", ""};
         }
 
-                // Muestra los detalles de la ruta seleccionada
+        return new String[]{puntoEncuentro, horaInicio, horaFin};
+    }
 
-        System.out.println("# ¡Excelente! La Ruta seleccionada tiene como punto de encuentro " + puntoEncuentro
-                + ", iniciando a las " + horaInicio + ", y termina a las " + horaFin + ".");
+    /**
+     * Muestra los detalles de la ruta seleccionada.
+     * 
+     * @param detallesRuta Un arreglo de Strings con los detalles de la ruta
+     *                     [punto de encuentro, hora de inicio, hora de fin].
+     */
+    public static void mostrarDetallesRuta(String[] detallesRuta) {
+        System.out.println("# ¡Excelente! La Ruta seleccionada tiene como punto de encuentro " + detallesRuta[0]
+                + ", iniciando a las " + detallesRuta[1] + ", y termina a las " + detallesRuta[2] + ".");
+    }
 
-         // Solicita el número de participantes y guías, y calcula el total de personas.
+    /**
+     * Solicita al usuario la cantidad de participantes o guías.
+     * 
+     * @param scanner El Scanner utilizado para leer la entrada del usuario.
+     * @param tipo    Un String que indica si se está solicitando la cantidad de
+     *                "participantes" o "guías".
+     * @return La cantidad ingresada de participantes o guías.
+     */
+    public static int solicitarCantidad(Scanner scanner, String tipo) {
+        System.out.println("# ¿Cuántos " + tipo + " acudirán a la caminata el día de hoy?");
+        return scanner.nextInt();
+    }
 
-        System.out.println("# ¿Cuántos participantes acudirán a la caminata el día de hoy?");
-        int participantes = scanner.nextInt();
-        System.out.println("# ¿Cuántos guías acudirán a la caminata el día de hoy?");
-        int guias = scanner.nextInt();
-        int totalPersonas = participantes + guias;
-
-        // Solicita y recibe la temperatura y humedad relativa
-
-
+    /**
+     * Solicita al usuario las condiciones meteorológicas (temperatura y humedad),
+     * y verifica si las condiciones son adecuadas para la caminata.
+     * 
+     * @param scanner El Scanner utilizado para leer la entrada del usuario.
+     */
+    public static void solicitarYVerificarCondicionesMeteorologicas(Scanner scanner) {
         System.out.println("# Ingresar la temperatura en grados centígrados °C");
         float temperatura = scanner.nextFloat();
+        
         System.out.println("# Ingresar el porcentaje de humedad relativa");
         float humedad = scanner.nextFloat();
-
-        // Verifica las condiciones meteorológicas y muestra un mensaje adecuado
 
         if (temperatura >= 20 && temperatura <= 25 && humedad >= 40 && humedad <= 60) {
             System.out.println("# ¡Hace un buen día para caminar por Cali!");
         }
-        // Calcula y muestra el número de buses necesarios para el transporte
+    }
 
-        int busesNecesarios = (totalPersonas + 24) / 25; 
+    /**
+     * Calcula el número de buses necesarios para transportar a todos los
+     * participantes y guías, y muestra el resultado.
+     * 
+     * @param totalPersonas La cantidad total de personas que participarán en la
+     *                      caminata.
+     */
+    public static void calcularYMostrarBuses(int totalPersonas) {
+        int busesNecesarios = (totalPersonas + 24) / 25;
         System.out.println("# Al ser un total de " + totalPersonas
                 + " personas que harán parte de la actividad, se necesitarán un total de: " + busesNecesarios
                 + " buses para llevarla a cabo de manera exitosa. ¡Nos vemos en la COP16!");
